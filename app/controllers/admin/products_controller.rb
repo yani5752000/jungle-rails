9c0f61c77461998cca13b8320829fbb9c0828ed8
Farid Asadpour <yani5752000@yahoo.com>
2021-08-25 01:10:27 +0000

    feature/testing-home-page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+
   def product_count
     @prod_count = Product.count
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+  # http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   protect_from_forgery with: :exception
 
